13f347d8f1abf90cccdcb8cbe0ef7847d451047c
Sravan Ks <microtechworkdesksravan@gmail.com>
2026-06-22 13:33:39 +0530

    test commit

--- lib/features/auth/dashboard/dashboard_constants.dart
+++ lib/features/auth/dashboard/dashboard_constants.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class DashboardConstants {
   static const Color brandBlue = Color(
     0xFF2300C4,
-  ); // same as LoginPage.brandBlue
+  );
   static const Color bg = Color(0xFFF6F7FB);
 
   static const double radiusLg = 26;
